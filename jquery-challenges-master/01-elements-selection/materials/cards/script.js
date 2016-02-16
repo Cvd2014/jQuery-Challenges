176397643721d74f56431d80753db6c902043b62
Ciaran Darcy <ciaran.darcy1@gmail.com>
2016-02-16 14:37:21 +0000

    completed challlenges 1-4

--- jquery-challenges-master/01-elements-selection/materials/cards/script.js
+++ jquery-challenges-master/01-elements-selection/materials/cards/script.js
@@ -12,4 +12,40 @@ $(document).ready(function() {
 		$(".card").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
+	$("p").on("click", function(){
+		$('p').css("color", "red");	
+	});
+	$('h2').hover( function(){
+		$("h2").css("color", "lightblue");
+	});
+	$("#html").hover(function(){
+		$("h2").removeClass("active");
+		$("#html").addClass("active")
+	});
+	$("#mySql").hover(function(){
+		$("h2").removeClass("active");
+		$("#mySql").addClass("active")
+	});
+	$("#python").hover(function(){
+		$("h2").removeClass("active");
+		$("#python").addClass("active")
+	});
+	$("#jQuery").hover(function(){
+		$("h2").removeClass("active");
+		$("#jQuery").addClass("active")
+	});
+	$("#css").hover(function(){
+		$("h2").removeClass("active");
+		$("#css").addClass("active")
+	});
+	$("#django").hover(function(){
+		$("h2").removeClass("active");
+		$("#django").addClass("active")
+	});
+	$(".bottom_button").hover(function(){
+		$("body").css("background-color", "black");},
+		function(){
+			$("body").css("background-color","grey");
+		});
+
 });
